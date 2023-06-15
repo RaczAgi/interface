fc3741d1f0dd15628d555b3bcbb1feb489e75dcd
Ági <raczagi07@gmail.com>
2023-06-15 23:03:01 +0200

    square

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
         Shape kor = new Circle(2);
         System.out.println("A kerulete: " + kor.getPerimeter());
         System.out.println("A terulete: " + kor.getArea());
+        Shape negyzet = new Square(3);
+        System.out.println("A kerülete: " + negyzet.getPerimeter());
+        System.out.println("A területe: " + negyzet.getArea());
     }
 }
